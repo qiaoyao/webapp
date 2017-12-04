83f505a784079d871b6c6b4144ac822a0db3bb17
qiaoyao <1246686104@qq.com>
2017-12-04 16:11:25 +0800

    1

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,5 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  API_HOST: '"http://192.168.1.172/"'
+  NODE_ENV: '"development"'
 })
