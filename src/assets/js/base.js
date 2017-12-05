2217f4a281b5ebc62fec940f46a3ea90c7d41e06
dany198x <dany198x@163.com>
2017-12-05 10:49:43 +0800

    联系我们

--- src/assets/js/base.js
+++ src/assets/js/base.js
@@ -14,6 +14,7 @@ export function computedREM() {
   const html = document.documentElement;
   const windowWidth = html.clientWidth;
   html.style.fontSize = parseInt(windowWidth / 750 * 20) + 'px';
+  return parseInt(windowWidth / 750 * 20);
 }
 
 /**
--- src/views/contact.vue
+++ src/views/contact.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="contact">
     <div class="map-box">
-      <div class="shop_map" id="container" style="width:1170px; height:455px;"></div>
+      <div class="shop_map" id="container"></div>
     </div>
     <div class="store-tab-group">
       <div class="store-tab" :class="{'checked' : currentTabIndex == 0}" @click="changeTab(0)">
@@ -36,6 +36,9 @@
 </template>
 
 <script>
+import {computedREM} from '../assets/js/base'
+const rem = computedREM();
+console.log(require("../assets/images/jindu.jpg"));
 const contact = {
   name: "contact",
   data() {
@@ -46,19 +49,19 @@ const contact = {
           address: "上海市闵行区金都路1618号",
           name: "允典居家总店（金都路店）",
           city: "上海市",
-          image: "images/jindu.jpg"
+          image: require("../assets/images/jindu.jpg")
         },
         {
           address: "上海市长宁区古北路786号",
           name: "允典居家总店（古北路店）",
           city: "上海市",
-          image: "images/gubei.jpg"
+          image: require("../assets/images/gubei.jpg")
         },
         {
           address: "北京市朝阳区北四环东路116号",
           name: "允典居家总店（北京北四环店）",
           city: "北京市",
-          image: "images/beijing.jpg"
+          image: require("../assets/images/beijing.jpg")
         }
       ],
       address: "",
@@ -74,19 +77,20 @@ const contact = {
   },
   mounted: function() {
     this.changeTab(-1);
-    this.haha();
+    this.initialize();
+    // this.haha();
   },
   methods: {
-    haha() {
-      this.$http
-        .post(this.$URL + "wap/test",{act:"dfdsjfkdsf"})
-        .then(function(response) {
-          console.log(response);
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
-    },
+    // haha() {
+    //   this.$http
+    //     .post(this.$URL + "wap/test",{act:"dfdsjfkdsf"})
+    //     .then(function(response) {
+    //       console.log(response);
+    //     })
+    //     .catch(function(error) {
+    //       console.log(error);
+    //     });
+    // },
     changeTab(index) {
       if (index == this.currentTabIndex) {
         return false;
@@ -103,17 +107,52 @@ const contact = {
       this.address = this.storeInfoList[index].address;
       this.store_name = this.storeInfoList[index].name;
       this.imageUrl = this.storeInfoList[index].image;
-      this.opts = { width: 384 };
+      this.opts = { width: 33*rem };
       this.sContent =
-        "<img style='float:left;' id='imgDemo' src='" +
+        "<img style='float:left;width:9rem;height:6rem;' id='imgDemo' src='" +
         this.imageUrl +
-        "' width='96' height='61'/>" +
-        "<div style='float:left;margin-top: 13px;margin-left:10px;color:#333;font-size:12px;'><p style='float:left;margin-bottom: 3px;'>店铺名称：" +
+        "'/>" +
+        "<div style='float:left;margin-top: 1.3rem;margin-left:1rem;color:#333;font-size:1.2rem;'><p style='float:left;margin-bottom: 0.3rem;'>店铺名称：" +
         this.store_name +
         "</p>" +
         "<p>详细地址：" +
         this.address +
         "</p></div>";
+        this.initialize();
+    },
+    initialize() {
+      var _this = this;
+        this.map = new BMap.Map("container");
+        var myGeo = new BMap.Geocoder();
+        // 将地址解析结果显示在地图上,并调整地图视野
+        myGeo.getPoint(this.address, function (point1) {
+            if (point1) {
+                _this.point = point1;
+                _this.map.enableScrollWheelZoom();
+                _this.map.addControl(new BMap.NavigationControl());
+                _this.map.addControl(new BMap.ScaleControl());
+                _this.map.addControl(new BMap.OverviewMapControl());
+                _this.setPoint(_this.point);
+            } else {
+                alert("您选择地址没有解析到结果!");
+            }
+        }, this.city);
+    },
+    setPoint(point) {
+        if (point) {
+            this.map.centerAndZoom(point, 15);
+            var marker = new BMap.Marker(point);
+            var infoWindow = new BMap.InfoWindow(this.sContent, this.opts);
+            marker.addEventListener("click", function () {
+                this.openInfoWindow(infoWindow);
+                //图片加载完毕重绘infowindow
+                document.getElementById('imgDemo').onload = function (){
+                    infoWindow.redraw();   //防止在网速较慢，图片未加载时，生成的信息框高度比图片的总高度小，导致图片部分被隐藏
+                }
+            });
+            this.map.addOverlay(marker);
+            marker.openInfoWindow(infoWindow);
+        }
     }
   }
 };
@@ -125,8 +164,14 @@ export default contact;
 .map-box {
   width: 100%;
   height: 45rem;
+  .shop_map {
+    width: 100%;
+    height: 100%;
+  }
 }
+
 .store-tab-group {
+  overflow: hidden;
   width: 100%;
   display: flex;
   margin-bottom: 1rem;
