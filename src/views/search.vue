79ef24a7afd045de3c23742348d9eca662797ab6
boo-boom <510126766@qq.com>
2017-12-06 10:12:35 +0800

    vuex

--- src/views/search.vue
+++ src/views/search.vue
@@ -1,11 +1,19 @@
 <template>
-  <div>123213</div>
+  <div>search</div>
 </template>
 
 <script>
+import { mapMutations } from "vuex";
+
 export default {
-  
-}
+  created() {
+    this.setShowHeader(false);
+    this.setShowFooter(false);
+  },
+  methods: {
+    ...mapMutations(["setShowHeader", "setShowFooter"])
+  }
+};
 </script>
 
 <style scoped lang="scss">
